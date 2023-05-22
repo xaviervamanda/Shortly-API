59c1ce1ca09275cc81f95ee17b4dc564e9f76727
Amanda Xavier <amandavxavier@gmail.com>
2023-05-22 19:48:31 -0300

    Fix: status code error (invalid token) route POST /urls/shorten

--- src/middlewares/authValidation.middleware.js
+++ src/middlewares/authValidation.middleware.js
@@ -9,11 +9,17 @@ export async function authValidation(req, res, next) {
     const { authorization } = req.headers;
     const token = authorization?.replace("Bearer ", "");
     if (!token) return res.sendStatus(401)
+    let data;
 
     const key = process.env.JWT_SECRET || "my_secret_key";
 
     try {
-        const data = jwt.verify(token, key);
+        data = jwt.verify(token, key);
+    } catch (tokenError) {
+        return res.status(401).send("Invalid token");
+    }
+
+    try {
         const user = await checkUserByEmail(data.email);
         const session = await checkUserSession(token);
         if (session.rowCount === 0 || user.rows[0].id !== session.rows[0].userId) return res.sendStatus(401)
